ad0d7a5ddf09e554223ffac4ff17ab3e7973d54c
Emirhan <125173937+DeVBulut@users.noreply.github.com>
2023-06-30 16:48:01 -0600

    Attack Reworked
    New Attack is a lot basic and better for development

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -8,12 +8,19 @@ public class PlayerCombat : MonoBehaviour
 {
     private Rigidbody2D rb;
     private PlayerController pController;
+    public Transform attackColliderPos_1;
+    public Transform attackColliderPos_2;
     private float cooldown = 0.25f;
     private float cooldownCounter;
+
+    private float AttackTimer;
+    private float AttackTimerCooldown = 0.3f;
+    private bool AttackTimerBool = false;
     public bool isAttacking;
     public bool isAttackingSlow;
     private bool canAttack = true;
     private float originalGravity;
+    private bool HitSquence;
     [SerializeField] private float attackSpeed = 500f;
     [SerializeField] private LayerMask HittableLayers;
     [Range(0, 10)][SerializeField] private int AttackDamage;                    
@@ -23,51 +30,59 @@ public class PlayerCombat : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         pController = GetComponent<PlayerController>();
         originalGravity = rb.gravityScale;
+        
     }
 
     void Update()
     {
         cooldownCounter = cooldownCounter - Time.deltaTime;
-        if(Input.GetKeyDown(KeyCode.Mouse0) && cooldownCounter < 0) {  Slash(); }
-
-
-        if(isAttackingSlow == true && pController.IsGrounded()){
-                isAttackingSlow = false;
+        if(AttackTimerBool == true){
+            AttackTimer = AttackTimer - Time.deltaTime;
         }
+        if(Input.GetKeyDown(KeyCode.Mouse0) && cooldownCounter < 0) {  Attack(); }
     }
 
-    public void Slash()
+    public void Attack()
     {
         cooldownCounter = cooldown;
         #region SlashMovement
 
         if(canAttack){
-            isAttacking = true;
-            // Calculate the direction from the player to the mouse position
-            Vector2 dashDirection = (GetPositionOfMouse() - transform.position).normalized;
-            // Apply the dash force to the player
+            isAttacking  = true;
+            rb.gravityScale *= 0f;
             rb.velocity = Vector2.zero;
-            rb.AddForce(dashDirection * attackSpeed, ForceMode2D.Impulse);
-            StartCoroutine(SlowDownAttack());
+            AttackTimerBool = true;
+            Slash();
+            StartCoroutine(ComboAttack());
+            while(AttackTimer > 0){
+                if(Input.GetKeyDown(KeyCode.Mouse0)){
+                    Slash();
+                    HitSquence = true;
+                    StartCoroutine(ComboAttack());
+                }
+            }
+            
         }
-        // else{
-        //     // Calculate the direction from the player to the mouse position
-        //     Vector2 dashDirection = (GetPositionOfMouse() - transform.position).normalized;
-        //     // Apply the dash force to the player
-        //     rb.AddForce(dashDirection, ForceMode2D.Impulse);
-        // }
 
         #endregion
 
     }
 
-    IEnumerator SlowDownAttack(){
-
-    yield return new WaitForSeconds(0.1f); // Adjust this delay as needed
-
-    isAttackingSlow = true;
-    isAttacking = false;
-
+    public void Slash(){
+        Vector2 attackPos_1 = new Vector2(attackColliderPos_1.position.x, attackColliderPos_1.position.y);
+        Vector2 attackPos_2 = new Vector2(attackColliderPos_2.position.x, attackColliderPos_2.position.y);
+        Collider2D[] objectsInCollider = Physics2D.OverlapAreaAll(attackPos_1, attackPos_2, HittableLayers);
+        foreach(var hittedObject in objectsInCollider){
+            EnemyHealth enemyHealth = hittedObject.GetComponent<EnemyHealth>();
+            enemyHealth.GetHit(5, this.transform.position);
+        }
+    }
+    IEnumerator ComboAttack(){
+        yield return new WaitForSeconds(0.3f);
+        if(HitSquence != true){
+            rb.gravityScale = originalGravity;
+            isAttacking = false;
+        }
     }
 
 
@@ -84,8 +99,11 @@ public class PlayerCombat : MonoBehaviour
     }
 
     private void OnDrawGizmos() {
-            Gizmos.color = Color.blue; 
-
+            Vector2 attackPos_1 = new Vector2(attackColliderPos_1.position.x, attackColliderPos_1.position.y);
+            Vector2 attackPos_2 = new Vector2(attackColliderPos_2.position.x, attackColliderPos_2.position.y);
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube((attackPos_1 + attackPos_2) * 0.5f, attackPos_2 - attackPos_1);
     }
 
 }
