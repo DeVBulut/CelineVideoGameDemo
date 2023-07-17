006d4579074f8a7d186c46ecff2958bfb99008c1
Emirhan <125173937+DeVBulut@users.noreply.github.com>
2023-07-17 16:57:48 -0600

    Still Working on Attack Code

--- Assets/SetAttackBehaviour.cs
+++ Assets/SetAttackBehaviour.cs
@@ -5,31 +5,38 @@ using UnityEngine;
 public class SetAttackBehaviour : StateMachineBehaviour
 {
     public string boolName;
-    public bool updateonState;
-    public bool updateOnStateMachine;
     public bool valueOnEnter, valueOnExit;
+    public short AttackSquence;
+    private bool canAttack;
 
 
     // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       if(updateonState){
-            animator.SetBool(boolName, valueOnEnter);
-       }
+            if(stateInfo.IsName("Attack_0"))
+            {
+                AttackSquence = 1;
+            }
+            else if(stateInfo.IsName("Attack_1"))
+            {
+                AttackSquence = 2;
+            }
+            else if(stateInfo.IsName("Attack_2"))
+            {
+                AttackSquence = 3;
+            }
     }
 
     // OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+    }
 
     // OnStateExit is called before OnStateExit is called on any state inside this state machine
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if(updateonState){
-            animator.SetBool(boolName, valueOnEnter);
-       }
+
     }
 
     // OnStateMove is called before OnStateMove is called on any state inside this state machine
@@ -47,16 +54,15 @@ public class SetAttackBehaviour : StateMachineBehaviour
     // OnStateMachineEnter is called when entering a state machine via its Entry Node
     override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
     {
-       if(updateOnStateMachine){
-       animator.SetBool(boolName, valueOnEnter);
-       }
+       
+        animator.SetBool(boolName, valueOnEnter);
     }
 
     // OnStateMachineExit is called when exiting a state machine via its Exit Node
     override public void OnStateMachineExit(Animator animator, int stateMachinePathHash)
     {
-        if(updateOnStateMachine){
-            animator.SetBool(boolName, valueOnExit);
-        }
+        AttackSquence = 0;        
+        animator.SetBool(boolName, valueOnExit);
+    
     }
 }
--- Assets/TestCode.cs
+++ Assets/TestCode.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class TestCode : MonoBehaviour
 {   
      public Animator animator;
+     public float countdownCounter;
      private void Update() {
+
         if(Input.GetKeyDown(KeyCode.F)){
                 animator.SetTrigger(AnimationStrings.AttackTrigger);
         }
-    }
 
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_0") || animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_1") || animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_2")){
+            if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.99){
+                Debug.Log("Cooldown");
+            }
+        }
+
+    }
 
    
 }
